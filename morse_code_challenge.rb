fdb39cdad4435d687a6c7be2bcb48e44321a9317
katarighe <m.ighe@hotmail.com>
2023-09-27 16:15:26 +0300

    Fix typo issues

--- morse_code_challenge.rb
+++ morse_code_challenge.rb
@@ -1,12 +1,12 @@
 # Decode a Morse code character
 def decode_char(character)
-  morse_alphabet = {
+  morse_code = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
     '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
     '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
     '-.--' => 'Y', '--..' => 'Z'
   }
-  morse_alphabet[character]
+  morse_code[character]
 end
 
-# Decode the character A
+# Decode the Morse code character A
